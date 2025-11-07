d5cad3182728d5a67e8520e74543703ce395e8c6
marco-giotto <marco.giotto-c@1111systems.com>
2025-11-07 14:49:45 -0300

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn Jenkins link', () => {
-  render(<App />);
+test("renders learn Jenkins link", async () => {
+  await render(<App />);
   const linkElement = screen.getByText(/learn Jenkins/i);
   expect(linkElement).toBeInTheDocument();
 });
